ace325f164d91a6423e764de3a40d189c3b13273
minsuk2609 <67671019+minsuk2609@users.noreply.github.com>
2023-04-12 00:15:37 -0600

    Added profile dropdown for the new pages added

--- pickup-ready-orders.php
+++ pickup-ready-orders.php
@@ -4,6 +4,18 @@
     if(!isset($_SESSION['username']) || $_SESSION['role'] == "Front"){
         header('location:signin.php');
     }
+    $emp = $_SESSION['Emp_id'];
+    $sql = "Select * from `employee` where Emp_id = '$emp'";
+    $result = mysqli_query($con, $sql);
+    $row=mysqli_fetch_assoc($result);
+    if($result){
+      $fname = $row['Fname'];
+      $lname = $row['Lname'];
+      $role = $row['role'];
+    }
+    else{
+      die(mysqli_error($con));
+    }
 ?>
 
 
@@ -15,11 +27,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Orders Ready for Pick up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="d-flex justify-content-between">
         <a href="back-home.php" class="btn btn-primary m-2">Back</a>
-        <a href="logout.php" class="btn btn-primary m-2">Logout</a>
+        <div class="dropdown m-2">
+          <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+          <?php echo $fname . " " . $lname;?>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <li><span class="dropdown-item-text"><strong><?php echo $role;?></strong></span></li>
+            <li><a class="dropdown-item" href="profile.php">Profile</a></li>
+            <li><a class="dropdown-item" style = "color: red;" href="logout.php">Logout</a></li>
+          </ul>
+        </div>
     </div>
 
     <h1 class="text-center mt-5">Orders Ready for Pick up</h1> 
